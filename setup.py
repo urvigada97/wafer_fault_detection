2e06b8b09bc948885797bf0be9ea4950cb892edf
Urvi Gada <urvigada96@gmail.com>
2021-03-30 18:51:31 +0530

    Read config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='It's a wafer fault detection project using mlops',
+    description='Its a wafer fault detection project using mlops',
     author='Urvi Gadda',
     license='MIT',
 )
