176ad6c2712a186831936e1f411fbefe0931bab1
Urvi Gada <urvigada96@gmail.com>
2021-03-30 10:21:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It's a wafer fault detection project using mlops',
+    author='Urvi Gadda',
+    license='MIT',
+)
